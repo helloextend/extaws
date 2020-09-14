67b29ab4e65099523b98d80ed853582b022651a0
Corey Ryan <corey@extend.com>
2020-09-14 02:11:30 -0700

    Fix export

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
 import program from 'commander'
+import { ExtAws } from './lib/extaws'
+
+export { ExtAws }
 
 program
   .version('0.0.1')
@@ -8,4 +11,3 @@ program
   .command('login [username] [password]', 'Login to AWS Hub account via Okta', {isDefault: true, executableFile: 'lib/login'})
   .command('clear', 'Clear locally stores information', {executableFile: 'lib/clear'})
   .parse(process.argv)
-
